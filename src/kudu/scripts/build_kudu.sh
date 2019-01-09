d26ac63f0ed94ee7f1e44922655825f90f21fdd7
laiyingchun <laiyingchun@xiaomi.com>
2019-01-09 19:27:16 +0800

    [script] Enhance build script to prepare minos package and move kudu-tools repo to scripts directory

---
+++ src/kudu/scripts/build_kudu.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+BASE_DIR="$( cd "$( dirname "$0"  )" && cd ../../.. && pwd )"
+
+#USAGE: copy_file src [src...] dest
+function copy_file() {
+  if [[ $# -lt 2 ]]; then
+    echo "ERROR: invalid copy file command: cp $*"
+    exit 1
+  fi  
+  cp -v $*
+  if [[ $? -ne 0 ]]; then
+    echo "ERROR: copy file failed: cp $*"
+    exit 1
+  fi  
+}
+
+KUDU_VERSION=`cat ${BASE_DIR}version.txt`
+OS=`lsb_release -d | awk '{print $2}'`
+echo "Start to build kudu $KUDU_VERSION on $OS"
+
+if [[ "$OS" == "CentOS" ]]; then
+  build-support/enable_devtoolset.sh
+  thirdparty/build-if-necessary.sh
+elif [[ "$OS" == "Ubuntu" ]]; then
+  thirdparty/build-if-necessary.sh
+else
+  echo "ERROR: unsupported OS: $OS in $0"
+  exit 1
+fi
+
+mkdir -p ${BASE_DIR}/build/release
+cd ${BASE_DIR}/build/release
+../../thirdparty/installed/common/bin/cmake -DCMAKE_BUILD_TYPE=release ../..
+make -j `cat /proc/cpuinfo | egrep "^processor\s:" | wc -l`
+if [[ $? -ne 0 ]]; then
+  echo "ERROR: build Kudu failed"
+  exit 1
+fi
+echo "Build Kudu succeed"
+
+VERSION_DEFINES=${BASE_DIR}/build/release/src/kudu/generated/version_defines.h
+if [[ ! -f ${VERSION_DEFINES} ]]; then
+  echo "ERROR: $VERSION_DEFINES not found"
+  exit 1
+fi
+
+CLEAN_REPO=`grep "^#define KUDU_BUILD_CLEAN_REPO " ${VERSION_DEFINES} | awk '{print $NF}' | tr 'A-Z' 'a-z'`
+if [[ "$CLEAN_REPO"x != "true"x ]]; then
+  echo "ERROR: repository is not clean"
+  exit 1
+fi
+
+VERSION=`grep "^#define KUDU_VERSION_STRING " ${VERSION_DEFINES} | cut -d "\"" -f 2`
+COMMIT_ID=`grep "^#define KUDU_GIT_HASH " ${VERSION_DEFINES} | cut -d "\"" -f 2`
+BUILD_TYPE=`grep "^#define KUDU_BUILD_TYPE " ${VERSION_DEFINES} | cut -d "\"" -f 2`
+PACK_VERSION=`echo ${VERSION}-${COMMIT_ID:0:7}-${OS}-${BUILD_TYPE} | tr 'A-Z' 'a-z'`
+PACK_NAME=kudu-${PACK_VERSION}
+
+echo "Starting package $PACK_NAME"
+PACK_DIR=${BASE_DIR}/build/${PACK_NAME}
+PACKAGE=${PACK_NAME}.tar.gz
+rm -rf ${PACK_DIR} ${BASE_DIR}/build/${PACKAGE}
+mkdir -p ${PACK_DIR}
+echo "Coping files to $PACK_DIR"
+copy_file ${BASE_DIR}/build/latest/bin/kudu-master ${PACK_DIR}/kudu_master
+copy_file ${BASE_DIR}/build/latest/bin/kudu-tserver ${PACK_DIR}/kudu_tablet_server
+copy_file ${BASE_DIR}/build/latest/bin/kudu ${PACK_DIR}/
+copy_file ${BASE_DIR}/src/kudu/scripts/kudu_metrics_collector_for_falcon.py ${PACK_DIR}/
+copy_file ${BASE_DIR}/src/kudu/scripts/logger.conf ${PACK_DIR}/
+copy_file -r ${BASE_DIR}/www ${PACK_DIR}/
+cd ${BASE_DIR}/build
+tar -czf ${PACKAGE} ${PACK_NAME}
+echo "Packaged $PACKAGE succeed"
+
+PACK_TEMPLATE=""
+if [[ -n "$MINOS_CONFIG_FILE" ]]; then
+  PACK_TEMPLATE=`dirname $MINOS_CONFIG_FILE`/xiaomi-config/package/kudu.yaml
+fi
+
+if [[ -f ${PACK_TEMPLATE} ]]; then
+  echo "Modifying $PACK_TEMPLATE ..."
+  sed -i "/^version:/c version: \"$PACK_VERSION\"" ${PACK_TEMPLATE}
+  sed -i "/^build:/c build: \"\.\/run.sh pack\"" ${PACK_TEMPLATE}
+  sed -i "/^source:/c source: \"$BASE_DIR/build\"" ${PACK_TEMPLATE}
+else
+  echo "ERROR: modify kudu.yaml failed"
+  exit 1
+fi
+
+echo "Done"
---
+++ src/kudu/scripts/minos_control_server.py
@@ -0,0 +1,120 @@
+#! /usr/bin/env python
+# coding=utf-8
+
+# A tool for restarting servers, typically to restart tservers in kudu cluster
+
+import sys
+import commands
+import time
+import json
+import re
+
+minos_client_path = ''  # minos client full path
+master_rpcs = ''        # master rpc addresses
+cluster = ''            # cluster name in minos config
+job = 'tablet_server'   # job name in minos config
+operate = 'stop'        # minos operate type, currently support: restart, stop
+tasks = range(0, 5)     # an int element list, e.g. '[n]' for a single node, or 'range(m, n)' for several nodes
+flags = ''              # minos flags, e.g. '--update_config' for updating config
+known_unhealth_nodes = set()
+#known_unhealth_nodes.add()    # it's ok to add some known unhealth nodes, e.g. some already stoped servers
+
+
+def get_host(host_port):
+    return host_port.split(':')[0]
+
+def is_cluster_health():
+    status, output = commands.getstatusoutput('kudu cluster ksck %s -consensus=false'
+                                              ' -ksck_format=json_compact -color=never'
+                                              ' -sections=MASTER_SUMMARIES,TSERVER_SUMMARIES,TABLE_SUMMARIES'
+                                              ' 2>/dev/null'
+                                              % master_rpcs)
+    unhealth_nodes = set()
+    if status == 0 or status == 256:
+        ksck_info = json.loads(output)
+        for master in ksck_info['master_summaries']:
+            if master['health'] != 'HEALTHY':
+                unhealth_nodes.add(get_host(master['address']))
+        for tserver in ksck_info['tserver_summaries']:
+            if tserver['health'] != 'HEALTHY':
+                unhealth_nodes.add(get_host(tserver['address']))
+        if 'table_summaries' in ksck_info:
+            for table in ksck_info['table_summaries']:
+                if table['health'] != 'HEALTHY':
+                    unhealth_nodes.add(table['name'])
+    else:
+        unhealth_nodes.add('mockone')
+
+    return unhealth_nodes
+
+
+def check_parameter(message, parameter, allow_empty = False):
+    print(message % parameter)
+    answer = sys.stdin.readline().strip('\n').lower()
+    if answer != 'y' and answer != '':
+        exit()
+    if (not allow_empty and
+        (not parameter or
+         (isinstance(parameter, list) and len(parameter) == 0) or
+         (isinstance(parameter, str) and parameter.strip() == ''))):
+        print(time_header() + 'You should provide a valid parameter')
+        exit()
+
+
+def wait_cluster_health():
+    print(time_header() + 'Wait cluster to be health ...')
+    nodes = is_cluster_health()
+    health = (len(nodes) == 0)
+    while not health:
+        health = True
+        for node in nodes:
+            if node not in known_unhealth_nodes:
+                health = False
+                print(time_header() + 'Unhealthy node: ' + node)
+                time.sleep(5)
+                nodes = is_cluster_health()
+                break
+
+
+def parse_node_from_minos_output(output):
+    host = ''
+    regex = re.compile('Stop task [0-9]+ of (tablet_server) on ([0-9a-z-.]+)\(0\).+')
+    match = regex.search(output)
+    if match is not None:
+        host = match.group(2)
+    return host
+
+
+def time_header():
+    return time.strftime("%Y-%m-%d %H:%M:%S ", time.localtime())
+
+
+check_parameter('You will operate on cluster: %s? (y/n)', cluster)
+check_parameter('The master rpc addresses are: %s? (y/n)', master_rpcs)
+check_parameter('You will operate on job: %s? (y/n)', job)
+check_parameter('You will operate on tasks: %s? (y/n)', tasks)
+check_parameter('The operate is: %s? (y/n)', operate)
+check_parameter('The extra flags are: %s? (y/n)', flags, True)
+check_parameter('The known unhealth nodes are: %s? (y/n)', ','.join(known_unhealth_nodes), True)
+
+wait_cluster_health()
+for task in tasks:
+    if not isinstance(task, int):
+        print(time_header() + '%s is not a valid integer task id' % str(task))
+        exit()
+
+    print(time_header() + 'Start to operate on task %d' % task)
+    status, output = commands.getstatusoutput(
+        '%s/deploy %s kudu %s --job %s --task %d --skip_confirm %s'
+        % (minos_client_path, operate, cluster, job, task, flags))
+    print(time_header() + 'operate status: ' + str(status))
+    print(output)
+    if operate == 'stop':
+        known_unhealth_nodes.add(parse_node_from_minos_output(output))
+
+    wait_cluster_health()
+
+    print(time_header() + '==========================')
+    time.sleep(10)
+
+print(time_header() + 'Complete sucessfully')
