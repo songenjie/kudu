c94126d20ade6eac0d1cf1dd2134d192f8fbc53a
Yingchun Lai <405403881@qq.com>
2019-02-17 22:39:36 +0800

    [tools] Add tool 'perf table_scan'
    This is useful to show row count and scanning time cost of
    tablets in a table, we can use it to check whether there is
    a row count skew in different tablets, or to check whether
    there is a long tail to scan different tablets.
    
    And also we do some refactor for tool 'perf loadgen' in this commit.
    
    Change-Id: Ibebed05d9a91ae3f126d3cf2d92f66852b253edd
    Reviewed-on: http://gerrit.cloudera.org:8080/12516
    Tested-by: Kudu Jenkins
    Reviewed-by: Adar Dembo <adar@cloudera.com>

--- src/kudu/tools/tool_action_table.cc
+++ src/kudu/tools/tool_action_table.cc
@@ -90,6 +90,7 @@ DEFINE_bool(list_tablets, false,
 DEFINE_bool(modify_external_catalogs, true,
             "Whether to modify external catalogs, such as the Hive Metastore, "
             "when renaming or dropping a table.");
+DECLARE_bool(show_values);
 DECLARE_string(tables);
 
 namespace kudu {
@@ -142,22 +143,11 @@ class TableLister {
 
 namespace {
 
-const char* const kTableNameArg = "table_name";
 const char* const kNewTableNameArg = "new_table_name";
 const char* const kColumnNameArg = "column_name";
 const char* const kAlterColumnTypeArg = "alter_type";
 const char* const kNewColumnNameArg = "new_column_name";
 const char* const kKeyArg = "primary_key";
-
-Status CreateKuduClient(const RunnerContext& context,
-                        client::sp::shared_ptr<KuduClient>* client) {
-  const string& master_addresses_str = FindOrDie(context.required_args,
-                                                 kMasterAddressesArg);
-  vector<string> master_addresses = Split(master_addresses_str, ",");
-  return KuduClientBuilder()
-             .master_server_addrs(master_addresses)
-             .Build(client);
-}
 
 Status DeleteTable(const RunnerContext& context) {
   const string& table_name = FindOrDie(context.required_args, kTableNameArg);
@@ -576,7 +566,9 @@ Status ScanTable(const RunnerContext &context) {
 
   const string& table_name = FindOrDie(context.required_args, kTableNameArg);
 
+  FLAGS_show_values = true;
   TableScanner scanner(client, table_name);
+  scanner.SetOutput(&cout);
   return scanner.Run();
 }
 
@@ -653,7 +645,6 @@ unique_ptr<Mode> BuildTableMode() {
       .AddOptionalParameter("fill_cache")
       .AddOptionalParameter("num_threads")
       .AddOptionalParameter("predicates")
-      .AddOptionalParameter("show_value")
       .AddOptionalParameter("tablets")
       .Build();
 
