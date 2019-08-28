b6ca54aef64874b839d60c55c0fd3e3ef8ad2186
Yingchun Lai <405403881@qq.com>
2019-08-28 20:47:10 +0800

    [pricing] use API to upload bills

--- src/kudu/scripts/cal_bill_daily.py
+++ src/kudu/scripts/cal_bill_daily.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 
 import commands
+import datetime
 from git import Repo
 import heapq
 import logging
@@ -17,11 +18,9 @@ import yaml
 
 g_ignore_db_set = ('system', 'lcsbinlog', 'default', 'zhangxu_test_kudu')
 g_month_path, g_month_data_path = kudu_utils.prepare_pricing_month_path()
-g_usage_result_filename = g_month_path + 'tbl_usage.csv'
 g_clusters_info_dict = yaml.load(open(kudu_utils.g_script_path + '/kudurc', 'r').read(), Loader=yaml.FullLoader)
 g_clusters_info = g_clusters_info_dict['clusters_info']
 g_commit_filenames = list()
-g_commit_filenames.append(g_usage_result_filename)
 g_git_repo_dir = ''
 
 
@@ -63,12 +62,12 @@ def add_org_size(dbtable, org, size, org_size_desc):
 def get_org_size_desc_from_olap(cluster_name, dbtable_size_dict, known_db_org_dict):
     db_org_dict = {}
     meta_table = 'system.kudu_table_owners'
-    cmd = '%s/kudu table scan @%s %s -show_value=true' \
+    cmd = '%s/kudu table scan @%s %s -show_values=true' \
           ' -columns=name,db,org 2>&1 | grep "(string name=\\\""'\
           % (kudu_utils.script_path(), cluster_name, meta_table)
     status, output = commands.getstatusoutput(cmd)
     if status != 0:
-        kudu_utils.LOG.error('Scan table %s error, status %d, output \n%s' % (meta_table, status, output))
+        kudu_utils.LOG.error('Scan table %s error, command %s, status %d, output \n%s' % (meta_table, cmd, status, output))
     else:
         for line in output.splitlines():
             match_obj = re.search(r'string name="(.*)", string db="(.*)", string org="(.*)"', line, re.M | re.I)
@@ -117,6 +116,10 @@ def get_cluster_stat_filename(date, cluster_name):
     return g_month_data_path + date + '_' + cluster_name
 
 
+def get_service_usage_filename(date):
+    return g_month_data_path + date + '_kudu_total'
+
+
 def collect_origin_usage_for_cluster(cluster_name, cluster_info):
     kudu_utils.LOG.info('Start to collect usage info for cluster %s' % cluster_name)
     # Output: db.table size
@@ -141,16 +144,17 @@ def collect_origin_usage_for_cluster(cluster_name, cluster_info):
 
     results = []
     date = time.strftime('%Y-%m-%d', time.localtime())
+    period = int(time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple()))
     for org, size_desc in org_size_desc.iteritems():
         result = dict()
-        result['date'] = date
-        result['service'] = 'kudu'
-        result['region'] = cluster_info['region']
-        result['charge_type'] = cluster_info['charge_type']
-        result['instance'] = cluster_info['instance']
+        result['period'] = period
+        result['service_name'] = 'kudu'
+        result['region_name'] = cluster_info['region']
+        result['charge_type_name'] = cluster_info['charge_type']
+        result['instance_name'] = cluster_info['instance']
         result['cluster'] = cluster_name
-        result['user_type'] = 'org' if org.find('CL') != -1 else 'kerberos'
-        result['user_id'] = org
+        result['account_type'] = 'org' if org.find('CL') != -1 else 'kerberos'
+        result['account'] = org
         result['usage'] = size_desc['size']
         result['charge_object'] = size_desc['desc'].top_k()
         results.append(result)
@@ -183,7 +187,7 @@ def collect_origin_usage_for_clusters(cluster_name_list):
         collect_origin_usage_for_cluster(cluster_name, cluster_info)
 
 
-def calc_usage_result(origin_usage_filename, usage_result_file):
+def calc_usage_result(origin_usage_filename, service_usage_file):
     kudu_utils.LOG.info('Start to process daily statistics file %s' % origin_usage_filename)
     if not os.path.exists(origin_usage_filename):
         kudu_utils.LOG.error('File not exist')
@@ -191,45 +195,41 @@ def calc_usage_result(origin_usage_filename, usage_result_file):
     with open(origin_usage_filename, 'r') as origin_usage_file:
         users_usage = json.load(origin_usage_file)
         for user_usage in users_usage:
-            usage_result_file.write('%s, %s, %s, %s, %s, %s, %s, %s, \'{"storage_bytes":%d}\', \'%s\'\n'
-                                    % (user_usage['date'],
-                                       user_usage['service'],
-                                       user_usage['region'],
-                                       user_usage['charge_type'],
-                                       user_usage['instance'],
+            service_usage_file.write('%s, %s, %s, %s, %s, %s, %s, %s, \'{"storage_bytes":%d}\', \'%s\'\n'
+                                    % (user_usage['period'],
+                                       user_usage['service_name'],
+                                       user_usage['region_name'],
+                                       user_usage['charge_type_name'],
+                                       user_usage['instance_name'],
                                        user_usage['cluster'],
-                                       user_usage['user_type'],
-                                       user_usage['user_id'],
+                                       user_usage['account_type'],
+                                       user_usage['account'],
                                        user_usage['usage'],
                                        json.dumps(user_usage['charge_object'])))
         origin_usage_file.close()
     kudu_utils.LOG.info('Write to file finished')
 
 
-def calc_usage_result_for_cluster(cluster_name, date):
-    with open(g_usage_result_filename, 'a') as usage_result_file:
-        origin_usage_filename = get_cluster_stat_filename(date, cluster_name)
-        calc_usage_result(origin_usage_filename, usage_result_file)
-        usage_result_file.close()
+def calc_usage_result_for_cluster(service_usage_file, cluster_name, date):
+    origin_usage_filename = get_cluster_stat_filename(date, cluster_name)
+    calc_usage_result(origin_usage_filename, service_usage_file)
 
 
 def calc_usage_result_for_clusters(cluster_name_list, date_list):
     for date in date_list:
-        for cluster_name in cluster_name_list:
-            cluster_info = get_cluster_info(cluster_name)
-            if not cluster_info:
-                continue
-            calc_usage_result_for_cluster(cluster_name, date)
-
-
-def create_usage_result_file_if_not_exist():
-    if not os.path.exists(g_usage_result_filename):
-        # Create new file
-        with open(g_usage_result_filename, 'w') as usage_result_file:
+        service_usage_filename = get_service_usage_filename(date)
+        with open(service_usage_filename, 'w') as service_usage_file:
             # Write header
-            usage_result_file.write('date, service, region, charge_type, instance, '
-                                    'cluster, user_type, user_id, usage, charge_object\n')
-            usage_result_file.close()
+            service_usage_file.write('period, service_name, region_name, charge_type_name, instance_name, '
+                              'cluster, account_type, account, usage, charge_object\n')
+            for cluster_name in cluster_name_list:
+                cluster_info = get_cluster_info(cluster_name)
+                if not cluster_info:
+                    continue
+                calc_usage_result_for_cluster(service_usage_file, cluster_name, date)
+            service_usage_file.close()
+        kudu_utils.upload_usage_data('append', service_usage_filename)
+        g_commit_filenames.append(service_usage_filename)
 
 
 def push_file_to_repo(filenames):
@@ -268,8 +268,6 @@ def main(argv=None):
         return
 
     collect_origin_usage_for_clusters(cluster_name_list)
-
-    create_usage_result_file_if_not_exist()
 
     # date_list = kudu_utils.get_date_list('2019-06-01', kudu_utils.get_date())
     date_list = [kudu_utils.get_date()]
--- src/kudu/scripts/kudu_utils.py
+++ src/kudu/scripts/kudu_utils.py
@@ -6,6 +6,7 @@ import dateutil.relativedelta
 import logging
 from logging.handlers import RotatingFileHandler
 import os
+import requests
 
 LOG = logging.getLogger()
 g_time = datetime.datetime.now()
@@ -76,6 +77,28 @@ def get_date_list(start, end, step=1, format="%Y-%m-%d"):
     strptime, strftime = datetime.datetime.strptime, datetime.datetime.strftime
     days = (strptime(end, format) - strptime(start, format)).days
     return [strftime(strptime(start, format) + datetime.timedelta(i), format) for i in xrange(0, days, step)]
+
+
+# method:
+#   append: 追加写入账单
+#   reload: 清空数据并重新写入账单
+def upload_usage_data(method, filename):
+    LOG.info('Start to report %s by %s mode' % (filename, method))
+    report_url = "http://production-cost.api.xiaomi.net/api/v1/data/upload"
+    token = "c2534683e5504ab4850c49873a36de61"
+    url = "%s?sk=%s&method=%s" % (report_url, token, method)
+    with open(filename, "rb") as f:
+        resp = requests.post(url, files={"file": f})
+        if resp.status_code == 200:
+            # 成功
+            details = resp.json()
+            if details['code'] == 0:
+                LOG.info('Succeed to report %s by %s mode' % (filename, method))
+            else:
+                LOG.fatal('Failed to report %s by %s mode, details: %s' % (filename, method, str(details).decode("unicode-escape")))
+        else:
+            # 失败
+            LOG.fatal('Report failed, code %d' % resp.status_code)
 
 
 g_script_path = script_path()
