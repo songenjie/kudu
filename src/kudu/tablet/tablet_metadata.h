234ca4b21a942456099785f8f42f04ae2a1b2824
Yingchun Lai <405403881@qq.com>
2019-10-15 16:50:04 +0800

    [tablet] Optimize TabletMetadata::CollectBlockIds efficiency
    Container size may reallocate and elements will be moved or copied
    several times if using std::vector and inserting elements at the
    beginning in TabletMetadata::CollectBlockIds, we'd better to insert
    elements at the end.
    
    We did some simple benchmarks for TabletMetadata::CollectBlockIds()
    operation in TestTabletMetadata.BenchmarkCollectBlockIds, compared
    with old version, result shows that inserting at the end provides a
    linear time cost when block count per RowSet or RowSet count
    increasing.
    And also, we ran benchmarks to compare std::vector with std::list
    and std::deque, both of them have worse efficiency.
    Result details as follow:
    (The first column is FLAGS_test_row_set_count/FLAGS_test_block_count_per_rs,
    the other columns show 10 times total TabletMetadata::CollectBlockIds()
    time cost, in millisecond.)
               vector          list    deque   vector
               (head insert)                   (rear insert)
    1000/1000  3464            687     334     321
    2000/1000  15238           1390    647     613
    4000/1000  75139           3057    1392    1212
    8000/1000  328808          6593    2805    2736
    4000/2000  159517          6488    2965    2462
    4000/4000  348471          11967   5513    5141
    4000/8000  -(too long)     23706   11704   10806
    
    Change-Id: I7ce853e35eb7dfa9f9a099e465ea8edfaa7c4aa9
    Reviewed-on: http://gerrit.cloudera.org:8080/14445
    Tested-by: Kudu Jenkins
    Reviewed-by: Adar Dembo <adar@cloudera.com>

--- src/kudu/tablet/tablet_metadata.h
+++ src/kudu/tablet/tablet_metadata.h
@@ -111,7 +111,7 @@ class TabletMetadata : public RefCountedThreadSafe<TabletMetadata> {
   static std::vector<BlockIdPB> CollectBlockIdPBs(
       const TabletSuperBlockPB& superblock);
 
-  std::vector<BlockId> CollectBlockIds();
+  BlockIdContainer CollectBlockIds();
 
   const std::string& tablet_id() const {
     DCHECK_NE(state_, kNotLoadedYet);
@@ -188,7 +188,7 @@ class TabletMetadata : public RefCountedThreadSafe<TabletMetadata> {
   //
   // Blocks are removed from this set after they are successfully deleted
   // in a call to DeleteOrphanedBlocks().
-  void AddOrphanedBlocks(const std::vector<BlockId>& block_ids);
+  void AddOrphanedBlocks(const BlockIdContainer& block_ids);
 
   // Mark the superblock to be in state 'delete_type', sync it to disk, and
   // then delete all of the rowsets in this tablet.
@@ -284,6 +284,7 @@ class TabletMetadata : public RefCountedThreadSafe<TabletMetadata> {
  private:
   friend class RefCountedThreadSafe<TabletMetadata>;
   friend class MetadataTest;
+  friend class TestTabletMetadataBenchmark;
 
   // Compile time assert that no one deletes TabletMetadata objects.
   ~TabletMetadata();
@@ -325,7 +326,7 @@ class TabletMetadata : public RefCountedThreadSafe<TabletMetadata> {
                               const RowSetMetadataVector& rowsets) const;
 
   // Requires 'data_lock_'.
-  void AddOrphanedBlocksUnlocked(const std::vector<BlockId>& block_ids);
+  void AddOrphanedBlocksUnlocked(const BlockIdContainer& block_ids);
 
   // Deletes the provided 'blocks' on disk.
   //
@@ -333,7 +334,7 @@ class TabletMetadata : public RefCountedThreadSafe<TabletMetadata> {
   // 'orphaned_blocks_' set.
   //
   // Failures are logged, but are not fatal.
-  void DeleteOrphanedBlocks(const std::vector<BlockId>& blocks);
+  void DeleteOrphanedBlocks(const BlockIdContainer& blocks);
 
   enum State {
     kNotLoadedYet,
